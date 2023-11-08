5df1d2ed0e1fe29577f5ad72ff10bb1807d09dbe
Daniil Kravchenko <daniil.kravchenko.92@yandex.ru>
2023-11-08 12:03:04 +0100

    simple paper component

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import TourCard from './components/TourCard';
+
+
+function App() {
+  return (
+    <div className="App">
+      <TourCard/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/TourCard.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Paper from '@mui/material/Paper';
+
+export default function TourCard() {
+  return (
+    <Paper elevation={3} >
+      Hello
+    </Paper>
+  )
+}
