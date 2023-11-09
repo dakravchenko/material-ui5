9cf0bc0fd976410e77704ad52e9032f330ba067f
Daniil Kravchenko <daniil.kravchenko.92@yandex.ru>
2023-11-09 14:35:45 +0100

    custom theme and providers

--- src/components/TourCard.js
+++ src/components/TourCard.js
@@ -5,18 +5,44 @@ import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import Rating from '@mui/material/Rating';
 import {AccessTime} from '@mui/icons-material';
+import {createTheme, ThemeProvider} from '@mui/material';
 
-export default function TourCard() {
+const theme = createTheme({
+    components: {
+        MuiTypography : {
+            variants: [
+                {
+                    props: {
+                        variant: "body2"
+                    },
+                    style: {
+                        fontSize: 11,
+                    }
+                },
+                  {
+                    props: {
+                        variant: "body3"
+                    },
+                    style: {
+                        fontSize: 9,
+                    }
+                }
+            ]
+        }
+    }
+})
+export default function TourCard({tour}) {
   return (
     <Grid item xs={12} sm={6} md={3} >
+        <ThemeProvider theme={theme}>
         <Paper elevation={3} >
             <img 
-                src="https://velvetescape.com/wp-content/uploads/2009/12/IMG_5455-1280x920.jpeg"
-                alt="some pic"
+                src={tour.image}
+                alt={tour.name}
             />
             <Box paddingX={1}>
                 <Typography variant="subtitle1" component="h2">
-                    Immerse into the Falls
+                    {tour.name}
                 </Typography>
                 <Box 
                     sx={{
@@ -25,33 +51,34 @@ export default function TourCard() {
                     }}>
                     <AccessTime sx={{width: 12.5}}/>
                     <Typography variant="body2" component="p" marginLeft={0.5}>
-                        5 hours
+                        {tour.duration} hours
                     </Typography>
                 </Box>
                 <Box
                     sx={{
-                        display: "flex",
+                        display: "flex", 
                         alignItems: "center"
                         }}
                         marginTop={3}
                         >
                     
                     <Rating name='read-only' value={4.5} precision={0.5} size='small'/>
-                    <Typography variant="body2" component="p" marginLeft={0.5}>
-                        4.5
+                    <Typography variant="body3" component="p" marginLeft={0.5}>
+                        {tour.rating}
                     </Typography>
-                    <Typography variant="body2" component="p" marginLeft={1.5}>
-                        (655 views)
+                    <Typography variant="body3" component="p" marginLeft={1.5}>
+                        ({tour.numberOfReviews} reviews)
                     </Typography>
                 </Box>
                 <Box>
                     <Typography variant="h6" component="h2" marginLeft={0}>
-                        From C $147
+                        From C ${tour.price}
                     </Typography>
                 </Box>
             </Box>
             
         </Paper>
+        </ThemeProvider>
     </Grid>
   )
 }
